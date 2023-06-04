ce16d1a1dcdb2f1b81ad62a0aee4873a0a641f43
Thanos Nikolaidis <121765261+Nikolaidis-CloudStories@users.noreply.github.com>
2023-06-05 00:37:14 +0300

    Add files via upload

---
+++ SPOnline export a spesific site members.ps1
@@ -0,0 +1,16 @@
+﻿Connect-SPOService -Url https://YouTenant-admin.sharepoint.com
+$siteUrl = "https://YouTenant.sharepoint.com/sites/YourSite"
+$users = Get-SPOUser -Site $siteUrl
+$accessReport = @()
+
+foreach ($user in $users) {
+    $userObject = New-Object PSObject
+    $userObject | Add-Member -MemberType NoteProperty -Name "LoginName" -Value $user.LoginName
+    $userObject | Add-Member -MemberType NoteProperty -Name "IsSiteAdmin" -Value $user.IsSiteAdmin
+    $userObject | Add-Member -MemberType NoteProperty -Name "Groups" -Value ($user.Groups | Out-String)
+    $accessReport += $userObject
+}
+
+$accessReport | Export-Csv -Path "C:\Path\To\Export\File.csv" -NoTypeInformation
+
+
